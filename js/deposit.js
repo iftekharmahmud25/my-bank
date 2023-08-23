f1288da236a6fb5c9397a4aab5d89d2d7c461511
iftekharmahmud25 <iftekharmahmud98@gmail.com>
2023-08-22 18:48:21 -0800

    add another features

--- js/deposit.js
+++ js/deposit.js
@@ -1,15 +1,24 @@
 document.getElementById('btn-deposit').addEventListener('click', function(){
 
 const depositField = document.getElementById('deposit-field');
-const depositAmount = depositField.value;
+const newDepositAmountString = depositField.value;
+const newDepositAmount = parseFloat(newDepositAmountString);
 
 const depositTotalElement = document.getElementById('deposit-total');
-const totalDeposit = depositTotalElement.innerText;
+const previousDepositTotalStaring = depositTotalElement.innerText;
+const previousDepositTotal = parseFloat(previousDepositTotalStaring);
+
+const currentDepositTotal = newDepositAmount + previousDepositTotal;
+
+ depositTotalElement.innerText   =  currentDepositTotal;
 
 
-depositTotalElement.innerText   =  depositAmount;
+const balanceTotalElement = document.getElementById('balance-total');
+const previousBalanceTotalString = balanceTotalElement.innerText;
+const previousBalanceTotal  = parseFloat(previousBalanceTotalString);
+const currentBalanceTotal = previousBalanceTotal + newDepositAmount;  
+balanceTotalElement.innerText = currentBalanceTotal;
 
 
-
-
+ depositField.value = '';
 })
